391f2968c0a2d12aa0c98521dc2b68fc2e10d9dc
akhier <akherthedragonhearted@gmail.com>
2016-08-27 17:03:06 -0400

    Switch to only checking keypress as it works now that we actually have a console
    console_check_for_keypress freezes the program if given False
    
    str(key.vk) will let you check for matches without using getattr

--- DragonKeys.py
+++ DragonKeys.py
@@ -1,30 +1,28 @@
 import libtcodpy
-from Panel import Panel
-from Console import Console
 
 
 if __name__ == '__main__':
-    SCREEN_WIDTH = 80
-    SCREEN_HEIGHT = 50
+    from Panel import Panel
+    from Console import Console
+    SCREEN_WIDTH = 40
+    SCREEN_HEIGHT = 25
     con = Console(SCREEN_WIDTH, SCREEN_HEIGHT, 'test')
     pan = Panel(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
     testkey = getattr(libtcodpy, 'KEY_UP')
     print(testkey)
     if testkey == libtcodpy.KEY_UP:
         print('true')
-
-    key = libtcodpy.Key()
-    mouse = libtcodpy.Mouse()
     while not con.is_window_closed:
         con.clear
-        libtcodpy.sys_check_for_event(libtcodpy.EVENT_KEY_PRESS |
-                                      libtcodpy.EVENT_MOUSE, key, mouse)
+        key = libtcodpy.console_check_for_keypress(True)
         if key.vk != libtcodpy.KEY_NONE:
             if key.vk != libtcodpy.KEY_CHAR:
                 pan.clear
-                pan.write(0, 0, 'test')
+                pan.write(0, 0, str(key.vk))
             else:
                 pan.clear
                 pan.write(0, 0, chr(key.c))
+        if key.vk == libtcodpy.KEY_ESCAPE:
+            break
         pan.blit()
         con.flush
