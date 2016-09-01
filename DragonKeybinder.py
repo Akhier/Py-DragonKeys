b03c7220ab38f034f4930cc34818c5f3666ebafe
akhier <akherthedragonhearted@gmail.com>
2016-09-01 00:54:32 -0400

    *scroll arrows now actual buttons using solid arrows instead of ^ and v
    +output button that was just changed last time
    
    *all buttons along the button justify left and space removed from words because of space constraints

--- DragonKeybinder.py
+++ DragonKeybinder.py
@@ -27,21 +27,37 @@ BTN['SAVE_AS'].write(1, 1, temp)
 temp = ''.ljust(SCREEN_WIDTH - BTN['SAVE_AS'].x2 - 3)
 BTN['NAME'] = Panel(BTN['SAVE_AS'].x2 + 1, 0, len(temp) + 2, 3, border=True)
 BTN['NAME'].write(1, 1, temp)
-temp = 'Edit Binding'
-BTN['EDIT_BIND'] = Panel(0, SCREEN_HEIGHT - 3, len(temp) + 2, 3, border=True)
+temp = 'AddBinding'
+BTN['ADD_BIND'] = Panel(0, SCREEN_HEIGHT - 3, len(temp) + 2, 3, border=True)
+BTN['ADD_BIND'].write(1, 1, temp)
+temp = 'EditBinding'
+BTN['EDIT_BIND'] = Panel(BTN['ADD_BIND'].x2 + 1, SCREEN_HEIGHT - 3,
+                         len(temp) + 2, 3, border=True)
 BTN['EDIT_BIND'].write(1, 1, temp)
-temp = 'Remove Binding'
+temp = 'RemoveBinding'
 BTN['REM_BIND'] = Panel(BTN['EDIT_BIND'].x2 + 1, SCREEN_HEIGHT - 3,
                         len(temp) + 2, 3, border=True)
 BTN['REM_BIND'].write(1, 1, temp)
-temp = 'New Output'
-BTN['NEW_OUT'] = Panel(SCREEN_WIDTH - len(temp) - 2, SCREEN_HEIGHT - 3,
+temp = 'AddOutput'
+BTN['ADD_OUT'] = Panel(BTN['REM_BIND'].x2 + 1, SCREEN_HEIGHT - 3,
                        len(temp) + 2, 3, border=True)
-BTN['NEW_OUT'].write(1, 1, temp)
+BTN['ADD_OUT'].write(1, 1, temp)
+temp = 'EditOutput'
+BTN['EDIT_OUT'] = Panel(BTN['ADD_OUT'].x2 + 1, SCREEN_HEIGHT - 3,
+                        len(temp) + 2, 3, border=True)
+BTN['EDIT_OUT'].write(1, 1, temp)
+temp = 'RemoveOutput'
+BTN['REM_OUT'] = Panel(BTN['EDIT_OUT'].x2 + 1, SCREEN_HEIGHT - 3,
+                       len(temp) + 2, 3, border=True)
+BTN['REM_OUT'].write(1, 1, temp)
+BTN['SCROLL_UP'] = Panel(SCREEN_WIDTH - 1, 3, 1, 1)
+BTN['SCROLL_UP'].write(0, 0, unichr(30))
+BTN['SCROLL_DOWN'] = Panel(SCREEN_WIDTH - 1, 2 + TEXT_HEIGHT, 1, 1)
+BTN['SCROLL_DOWN'].write(0, 0, unichr(31))
 textpanel = Panel(0, 3, TEXT_WIDTH, TEXT_HEIGHT, border=True)
 workpanel = Panel(1, 1, TEXT_WIDTH - 2, TEXT_HEIGHT - 2)
 bindingspanel = Panel(0, 0, TEXT_WIDTH - 2, 1)
-scrollpanel = Panel(SCREEN_WIDTH - 1, 3, 1, SCREEN_HEIGHT - 6)
+scrollpanel = Panel(SCREEN_WIDTH - 1, 4, 1, SCREEN_HEIGHT - 8)
 mouse = libtcodpy.Mouse()
 key = libtcodpy.Key()
 toprow = 0
@@ -111,8 +127,6 @@ while not con.is_window_closed:
     textpanel.clear
     scrollpanel.clear
     bindingspanel.clear
-    scrollpanel.write(0, 0, '^')
-    scrollpanel.write(0, TEXT_HEIGHT - 1, 'v')
     libtcodpy.sys_check_for_event(libtcodpy.EVENT_KEY_PRESS |
                                   libtcodpy.EVENT_MOUSE,
                                   key, mouse)
