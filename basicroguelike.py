cb6d6f2abf98af26914120e6125c58a7dc030f66
akhier <akherthedragonhearted@gmail.com>
2016-09-01 22:19:07 -0400

    +binding csv for all basicroguelike commands
    *basicroguelike now using the keyhandler

--- basicroguelike.py
+++ basicroguelike.py
@@ -756,60 +756,113 @@ def handle_keys():
     global key
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-    elif key.vk == libtcod.KEY_ESCAPE:
-        return 'exit'
+    # elif key.vk == libtcod.KEY_ESCAPE:
+    #     return 'exit'
+    # if game_state == 'playing':
+    #     if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
+    #         player_move_or_attack(0, -1)
+    #     elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
+    #         player_move_or_attack(0, 1)
+    #     elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
+    #         player_move_or_attack(-1, 0)
+    #     elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
+    #         player_move_or_attack(1, 0)
+    #     elif key.vk == libtcod.KEY_HOME or key.vk == libtcod.KEY_KP7:
+    #         player_move_or_attack(-1, -1)
+    #     elif key.vk == libtcod.KEY_PAGEUP or key.vk == libtcod.KEY_KP9:
+    #         player_move_or_attack(1, -1)
+    #     elif key.vk == libtcod.KEY_END or key.vk == libtcod.KEY_KP1:
+    #         player_move_or_attack(-1, 1)
+    #     elif key.vk == libtcod.KEY_PAGEDOWN or key.vk == libtcod.KEY_KP3:
+    #         player_move_or_attack(1, 1)
+    #     elif key.vk == libtcod.KEY_KP5:
+    #         pass
+    #     else:
+    #         key_char = chr(key.c)
+    #         if key_char == 'g':
+    #             for object in objects:
+    #                 if object.x == player.x and \
+    #                    object.y == player.y and object.item:
+    #                     object.item.pick_up()
+    #                     break
+    #         if key_char == 'i':
+    #             chosen_item = inventory_menu('Press the key next to an ' +
+    #                                          'item to use it, or any ' +
+    #                                          'other to cancel.\n')
+    #             if chosen_item is not None:
+    #                 chosen_item.use()
+    #         if key_char == 'd':
+    #             chosen_item = inventory_menu('Press the key next to an ' +
+    #                                          'to drop it, or any ' +
+    #                                          'other to cancel.\n')
+    #             if chosen_item is not None:
+    #                 chosen_item.drop()
+    #         if key_char == 'c':
+    #             level_up_xp = LEVEL_UP_BASE + player.level * LEVEL_UP_FACTOR
+    #             msgbox('Character information\n\nLevel: ' + str(player.level) +
+    #                    '\nExperiance: ' + str(player.fighter.xp) +
+    #                    '\nExperiance to level up: ' + str(level_up_xp) +
+    #                    '\nMaximum HP: ' + str(player.fighter.max_hp) +
+    #                    '\nAttack: ' + str(player.fighter.power) +
+    #                    '\nDefense: ' + str(player.fighter.defense),
+    #                    CHARACTER_SCREEN_WIDTH)
+    #         if key_char == '<':
+    #             if stairs.x == player.x and stairs.y == player.y:
+    #                 next_level()
+    #         return 'didnt-take-turn'
+
+    output = binding.check_key(key)
+    if output == 'exit':
+        return output
     if game_state == 'playing':
-        if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
+        if output == 'didnt-take-turn':
+            return output
+        elif output == 'attackup':
             player_move_or_attack(0, -1)
-        elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
+        elif output == 'attackdown':
             player_move_or_attack(0, 1)
-        elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
+        elif output == 'attackleft':
             player_move_or_attack(-1, 0)
-        elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
+        elif output == 'attackright':
             player_move_or_attack(1, 0)
-        elif key.vk == libtcod.KEY_HOME or key.vk == libtcod.KEY_KP7:
+        elif output == 'attackupleft':
             player_move_or_attack(-1, -1)
-        elif key.vk == libtcod.KEY_PAGEUP or key.vk == libtcod.KEY_KP9:
+        elif output == 'attackupright':
             player_move_or_attack(1, -1)
-        elif key.vk == libtcod.KEY_END or key.vk == libtcod.KEY_KP1:
+        elif output == 'attackdownleft':
             player_move_or_attack(-1, 1)
-        elif key.vk == libtcod.KEY_PAGEDOWN or key.vk == libtcod.KEY_KP3:
+        elif output == 'attackdownright':
             player_move_or_attack(1, 1)
-        elif key.vk == libtcod.KEY_KP5:
-            pass
-        else:
-            key_char = chr(key.c)
-            if key_char == 'g':
-                for object in objects:
-                    if object.x == player.x and \
-                       object.y == player.y and object.item:
-                        object.item.pick_up()
-                        break
-            if key_char == 'i':
-                chosen_item = inventory_menu('Press the key next to an ' +
-                                             'item to use it, or any ' +
-                                             'other to cancel.\n')
-                if chosen_item is not None:
-                    chosen_item.use()
-            if key_char == 'd':
-                chosen_item = inventory_menu('Press the key next to an ' +
-                                             'to drop it, or any ' +
-                                             'other to cancel.\n')
-                if chosen_item is not None:
-                    chosen_item.drop()
-            if key_char == 'c':
-                level_up_xp = LEVEL_UP_BASE + player.level * LEVEL_UP_FACTOR
-                msgbox('Character information\n\nLevel: ' + str(player.level) +
-                       '\nExperiance: ' + str(player.fighter.xp) +
-                       '\nExperiance to level up: ' + str(level_up_xp) +
-                       '\nMaximum HP: ' + str(player.fighter.max_hp) +
-                       '\nAttack: ' + str(player.fighter.power) +
-                       '\nDefense: ' + str(player.fighter.defense),
-                       CHARACTER_SCREEN_WIDTH)
-            if key_char == '<':
-                if stairs.x == player.x and stairs.y == player.y:
-                    next_level()
-            return 'didnt-take-turn'
+        elif output == 'grab':
+            for object in objects:
+                if object.x == player.x and \
+                   object.y == player.y and object.item:
+                    object.item.pick_up()
+                    break
+        elif output == 'inventory':
+            chosen_item = inventory_menu('Press the key next to an ' +
+                                         'item to use it, or any ' +
+                                         'other to cancel.\n')
+            if chosen_item is not None:
+                chosen_item.use()
+        elif output == 'drop':
+            chosen_item = inventory_menu('Press the key next to an ' +
+                                         'to drop it, or any ' +
+                                         'other to cancel.\n')
+            if chosen_item is not None:
+                chosen_item.drop()
+        elif output == 'character':
+            level_up_xp = LEVEL_UP_BASE + player.level * LEVEL_UP_FACTOR
+            msgbox('Character information\n\nLevel: ' + str(player.level) +
+                   '\nExperiance: ' + str(player.fighter.xp) +
+                   '\nExperiance to level up: ' + str(level_up_xp) +
+                   '\nMaximum HP: ' + str(player.fighter.max_hp) +
+                   '\nAttack: ' + str(player.fighter.power) +
+                   '\nDefense: ' + str(player.fighter.defense),
+                   CHARACTER_SCREEN_WIDTH)
+        elif output == 'nextlevel':
+            if stairs.x == player.x and stairs.y == player.y:
+                next_level()
 
 
 def check_level_up():
