b2e95ce8db72903f3a8750321638e7554de9fb67
akhier <akherthedragonhearted@gmail.com>
2016-09-05 15:42:32 -0400

    -Fullscreen because DragonKeys doesn't yet have the ability to detect modifier keys
    *Main game loop now using check_keypress instead of sys_check_for_event then using check_key

--- basicroguelike.py
+++ basicroguelike.py
@@ -700,8 +700,8 @@ def msgbox(text, width=50):
 
 def handle_keys():
     global key
-    if key.vk == libtcod.KEY_ENTER and key.lalt:
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+    # if key.vk == libtcod.KEY_ENTER and key.lalt:
+    #     libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
     # elif key.vk == libtcod.KEY_ESCAPE:
     #     return 'exit'
     # if game_state == 'playing':
@@ -745,7 +745,8 @@ def handle_keys():
     #                 chosen_item.drop()
     #         if key_char == 'c':
     #             level_up_xp = LEVEL_UP_BASE + player.level * LEVEL_UP_FACTOR
-    #             msgbox('Character information\n\nLevel: ' + str(player.level) +
+    #             msgbox('Character information\n\nLevel: ' +
+    #                    str(player.level) +
     #                    '\nExperiance: ' + str(player.fighter.xp) +
     #                    '\nExperiance to level up: ' + str(level_up_xp) +
     #                    '\nMaximum HP: ' + str(player.fighter.max_hp) +
@@ -756,46 +757,44 @@ def handle_keys():
     #             if stairs.x == player.x and stairs.y == player.y:
     #                 next_level()
     #         return 'didnt-take-turn'
-
-    output = binding.check_key(key)
-    if output == 'exit':
-        return output
+    if key == 'exit':
+        return key
     if game_state == 'playing':
-        if output == 'didnt-take-turn':
-            return output
-        elif output == 'pass':
-            return output
-        elif 'attack' in output:
+        if key == 'didnt-take-turn':
+            return key
+        elif key == 'pass':
+            return key
+        elif 'attack' in key:
             x = 0
             y = 0
-            if 'up' in output:
+            if 'up' in key:
                 y = -1
-            elif 'down' in output:
+            elif 'down' in key:
                 y = 1
-            if 'left' in output:
+            if 'left' in key:
                 x = -1
-            elif 'right' in output:
+            elif 'right' in key:
                 x = 1
             player_move_or_attack(x, y)
-        elif output == 'grab':
+        elif key == 'grab':
             for object in objects:
                 if object.x == player.x and \
                    object.y == player.y and object.item:
                     object.item.pick_up()
                     break
-        elif output == 'inventory':
+        elif key == 'inventory':
             chosen_item = inventory_menu('Press the key next to an ' +
                                          'item to use it, or any ' +
                                          'other to cancel.\n')
             if chosen_item is not None:
                 chosen_item.use()
-        elif output == 'drop':
+        elif key == 'drop':
             chosen_item = inventory_menu('Press the key next to an ' +
                                          'to drop it, or any ' +
                                          'other to cancel.\n')
             if chosen_item is not None:
                 chosen_item.drop()
-        elif output == 'character':
+        elif key == 'character':
             level_up_xp = LEVEL_UP_BASE + player.level * LEVEL_UP_FACTOR
             msgbox('Character information\n\nLevel: ' + str(player.level) +
                    '\nExperiance: ' + str(player.fighter.xp) +
@@ -804,7 +803,7 @@ def handle_keys():
                    '\nAttack: ' + str(player.fighter.power) +
                    '\nDefense: ' + str(player.fighter.defense),
                    CHARACTER_SCREEN_WIDTH)
-        elif output == 'nextlevel':
+        elif key == 'nextlevel':
             if stairs.x == player.x and stairs.y == player.y:
                 next_level()
 
@@ -860,9 +859,10 @@ def monster_death(monster):
 
 
 def target_tile(max_range=None):
-    global key, mouse
+    global mouse
     while True:
         root.flush
+        key = libtcod.Key()
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS |
                                     libtcod.EVENT_MOUSE, key, mouse)
         render_all()
@@ -1052,9 +1052,10 @@ def play_game():
     mouse = libtcod.Mouse()
     key = libtcod.Key()
     while not root.is_window_closed:
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS |
-                                    libtcod.EVENT_MOUSE,
-                                    key, mouse)
+        # libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS |
+        #                             libtcod.EVENT_MOUSE,
+        #                             key, mouse)
+        key = binding.check_keypress()
         render_all()
 
         root.flush
